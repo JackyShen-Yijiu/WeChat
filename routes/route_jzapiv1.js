6c4a91b68d57ec484faddeb9eeb20a834581ad27
v-yafeili <v-yafeili@outlook.com>
2016-03-08 16:09:57 +0800

    增加文件目录结构体

---
+++ routes/route_jzapiv1.js
@@ -0,0 +1,7 @@
+/**
+ * Created by v-yaf_000 on 2016/3/8.
+ */
+var express=require("express");
+var Router=express.Router();
+
+module.exports = Router;
---
+++ routes/route_weixin.js
@@ -0,0 +1,10 @@
+/**
+ * Created by v-yaf_000 on 2016/3/8.
+ */
+var express=require("express");
+var Router=express.Router();
+var weixincontroller=require("../server/controller/controller_weixin");
+
+
+
+module.exports = Router;
