5578970c843b8150e4ac157d7f7ca584e8d2d7c1
majinqiang <1227208827@qq.com>
2016-03-09 15:07:17 +0800

    update server.js

--- server.js
+++ server.js
@@ -22,6 +22,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type, Authorization');
+  if (req.method.toUpperCase() === 'OPTIONS') {
+    return res.end();
+  }
+  next();
+});
 app.use("/weixin",weinxinRouter);
 app.use("/jzapi/v1",apiRouter);
 // server render
--- server/controller/controller_jzapiv1.js
+++ server/controller/controller_jzapiv1.js
@@ -18,7 +18,6 @@ exports.searchSchool = function (req, res) {
     var q = {
         latitude: parseFloat(req.query.latitude),
         longitude: parseFloat(req.query.longitude),
-        licensetype: req.query.licensetype ? req.query.licensetype : "",
         ordertype: req.query.ordertype ? parseInt(req.query.ordertype) : 0,
         index: req.query.index ? parseInt(req.query.index) : 1,
         count: req.query.count ? parseInt(req.query.count) : 1,
